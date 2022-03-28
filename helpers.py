17683ddfa3b0d0105051553040427bb2ea8d4e8f
Marc Lewis <marc8@live.unc.edu>
2022-03-28 13:01:31 -0400

    Merge fixes

--- helpers.py
+++ helpers.py
@@ -3,19 +3,11 @@ class todo:
     title: str
     description: str
     color: str
-<<<<<<< Updated upstream
-    unchecked: bool
-=======
     checked: bool
->>>>>>> Stashed changes
 
     def __init__(self, id: int, title: str, description: str, color: str):
         self.id = id
         self.title = title
         self.description = description
         self.color = color
-<<<<<<< Updated upstream
-        self.unchecked = True
-=======
         self.checked = False
->>>>>>> Stashed changes
